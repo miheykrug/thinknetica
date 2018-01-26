ae004a92a8dceb44f9df49bf0e00947c27259dce
Mikhail Kruglik <miheykrug@mail.ru>
2018-01-26 19:07:42 +0300

    fix3 Lesson 3

--- lesson3/cart.rb
+++ lesson3/cart.rb
@@ -4,13 +4,13 @@ loop do
   puts "Введите название товара"
   item = gets.chomp
   break if item == "stop"
-  
+
   puts "Введите цену товара"
   price = gets.chomp.to_f
-  
+
   puts "Введите количество товара"
   quantity = gets.chomp.to_i
-  
+
   prod = { price: price, quantity: quantity}
   cart[item] = prod
 end
@@ -18,8 +18,8 @@ end
 orpder_price = 0
 
 cart.each do |name, prod|
-  puts "Общая стоимость товара #{name}: #{prod.values.reduce(1, :*)}"
-  orpder_price += prod.values.reduce(1, :*)
+  puts "Общая стоимость товара #{name}: #{prod[:price] * prod[:quantity]}"
+  orpder_price += prod[:price] * prod[:quantity]
 end
 
 puts "Общая сумма всех покупок: #{orpder_price}"
