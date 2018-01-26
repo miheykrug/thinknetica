68fc1e7dcba020a433f717f32f463ac441d932e8
Mikhail Kruglik <miheykrug@mail.ru>
2018-01-26 15:37:41 +0300

    fix Lesson 3

--- lesson3/array_fibonacci.rb
+++ lesson3/array_fibonacci.rb
@@ -1,20 +1,10 @@
-fib1 = [1, 1]
-
-for i in 2..99
-  fib1[i] = fib1[i-1] + fib1[i-2]
-end
-
-puts fib1.to_s
-puts fib1.length
-
-fib2 = [1, 1]
+fib = [1, 1]
 j = 1
-loop do
+sum = 1
+while sum < 100
+  fib[j] = sum
   j += 1
-  summ = fib2[j-1] + fib2[j-2]
-  break if summ > 100
-  fib2[j] = summ
+  sum = fib[j-1] + fib[j-2]
 end
 
-puts fib2.to_s
-puts fib2.length
+puts fib.to_s
--- lesson3/cart.rb
+++ lesson3/cart.rb
@@ -1,6 +1,5 @@
 puts 'Для выхода введите "stop" '
 cart = {}
-orpder_price = 0
 loop do
   puts "Введите название товара"
   item = gets.chomp
@@ -14,10 +13,21 @@ loop do
   
   prod = { price: price, quantity: quantity}
   cart[item] = prod
-  prod_price = price * quantity
-  orpder_price +=prod_price
-
-  puts "Общая цена товара равна #{prod_price}"
 end
+
+orpder_price = 0
+
+cart.each do |name, prod|
+  prod_price = 1
+  
+  prod.each do |k, v|
+    prod_price *= v
+  end
+  
+  puts "Общая стоимость товара #{name}: #{prod_price}"
+  
+  orpder_price += prod_price
+end
+
 puts "Общая сумма всех покупок: #{orpder_price}"
 puts cart
