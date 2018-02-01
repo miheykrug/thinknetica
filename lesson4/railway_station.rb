2764d719597cd9cd0e5f36e0820cc3d7d595e5d5
Mikhail Kruglik <miheykrug@mail.ru>
2018-02-01 12:25:38 +0300

    fix Lesson 4

--- lesson4/railway_station.rb
+++ lesson4/railway_station.rb
@@ -2,8 +2,7 @@ puts "Возможные типы поездов: freight - грузовые; pa
 
 class Station
 
-  attr_accessor :trains
-  attr_reader :station_name
+  attr_reader :station_name, :trains
 
   def initialize(station_name)
     @station_name = station_name
@@ -12,110 +11,129 @@ class Station
 
   def take_train(train)
     @trains << train
-    puts "Поезд № #{train.number} прибыл на станцию #{self.station_name}"
-  end
-  def get_trains
-    puts "Сейчас на станции следующие поезда:"
-    self.trains.each { |train| puts "- поезд № #{train.number}, тип: #{train.type}, количесво вагонов: #{train.wagons}"}
+    puts "Поезд № #{train.number} прибыл на станцию #{station_name}"
   end
 
-  def get_types_of_trains
-    freight = 0
-    passenger = 0
-    self.trains.each { |train| train.type == "freight" ? freight += 1 : passenger += 1 }
-    puts "На станции #{freight} грузовых и #{passenger} пассажирских поездов"
+  def get_types_of_trains(type)
+    trains.each { |train| train.type == type }
   end
 
   def train_departure(train)
-    if self.trains.include?(train)
-      self.trains.delete(train)
-      puts "Поезд № #{train.number} отправился со станции #{self.station_name}"
+    if trains.include?(train)
+      trains.delete(train)
+      puts "Поезд № #{train.number} отправился со станции #{station_name}"
     else
-      puts "Такого поезда нет на станции #{self.station_name}"
+      puts "Такого поезда нет на станции #{station_name}"
     end
-
   end
+
 end
 
 class Route
-  attr_reader :first_station, :end_station
+
   attr_accessor :stations
 
   def initialize(first_station, end_station)
-    @stations = []
-    @stations << first_station << end_station
+    @stations = [] << first_station << end_station
   end
 
   def add_station(station)
-    self.stations.insert(-2, station)
+    stations.insert(-2, station)
   end
 
   def remove_station(station)
-    self.stations.delete(station)
+    if station == stations.first || station == stations.last
+      puts "Можно удалять только промежуточные станции"
+    else
+      stations.delete(station)
+    end
   end
 
-  def get_stations
+  def list_stations
     puts "На данном маршруте следующие остановки:"
-    self.stations.each { |station| puts station.station_name }
+    stations.each { |station| puts station.station_name }
   end
 
 end
 
 class Train
 
-  attr_reader :number, :type
-  attr_accessor :wagons, :speed, :route, :current_station
+  attr_reader :number, :type, :wagons, :route, :speed
 
   def initialize(number, type, wagons)
     @number = number
     @type = type
     @wagons = wagons
+    @speed = 0
+  end
+
+
+  def increase_speed(value)
+    self.speed += value
+  end
+
+  def reduse_speed(value)
+    self.speed -= value
+    self.speed = 0 if self.speed < 0
   end
 
   def stop
     self.speed = 0
   end
+
   def add_wagon
-    if self.speed == 0
-      @wagons += 1
+    if speed == 0
+      self.wagons += 1
     else
       puts "Нельзя прицепить вагон во время движения поезда"
     end
   end
 
   def remove_wagon
-    if self.speed == 0
-      @wagons -= 1
+    if speed == 0
+      if wagons == 0
+        puts "Невозможно отцепить вагон, т.к. вагонов не осталось"
+      else
+        self.wagons -= 1
+      end
     else
       puts "Нельзя отцепить вагон во время движения поезда"
     end
   end
 
+
   def add_route(route)
+    @@i = 0
     self.route = route
-    self.current_station = route.stations[0]
-    self.current_station.take_train(self)
+    route.stations[@@i].take_train(self)
   end
 
-  @@i = 0
   def to_next_station
-    self.current_station.train_departure(self)
+    route.stations[@@i].train_departure(self)
     @@i += 1
-    self.current_station = self.route.stations[@@i]
-    self.current_station.take_train(self)
-  end
-  def to_previous_station
-    self.current_station.train_departure(self)
-    @@i -= 1
-    self.current_station = self.route.stations[@@i]
-    self.current_station.take_train(self)
+    route.stations[@@i].take_train(self)
   end
 
-  def get_current_station
-    puts "Предыдущая станция: #{self.route.stations[@@i - 1].station_name}"
-    puts "Текущая станция: #{self.current_station.station_name}"
-    puts "Следующая станция: #{self.route.stations[@@i + 1].station_name}"
+  def to_previous_station
+    route.stations[@@i].train_departure(self)
+    @@i -= 1
+    route.stations[@@i].take_train(self)
   end
+
+  def previous_station
+    route.stations[@@i - 1]
+  end
+
+  def current_station
+    route.stations[@@i]
+  end
+
+  def next_station
+    route.stations[@@i + 1]
+  end
+
+  private
+    attr_writer :wagons, :route, :speed
 end
 
 
@@ -133,44 +151,52 @@ train2 = Train.new(2, "passenger", 8)
 
 station1.take_train(train1)
 station1.take_train(train2)
-station1.get_trains
-station1.get_types_of_trains
+station1.trains
+station1.get_types_of_trains("freight")
 station1.train_departure(train1)
 station1.train_departure(train1)
-station1.get_trains
-station1.get_types_of_trains
+station1.trains
+station1.get_types_of_trains("passenger")
 
 puts "--------------"
 
 route1.add_station(station3)
 route1.add_station(station4)
-route1.get_stations
+route1.list_stations
 route1.remove_station(station4)
-route1.get_stations
+route1.remove_station(station1)
+route1.list_stations
 route1.add_station(station4)
 
 puts "--------------"
 train3 = Train.new(3, "passenger", 10)
+train4 = Train.new(4, "passenger", 0)
 
-puts train3.speed = 30
 train3.add_wagon
 train3.stop
 train3.add_wagon
 puts train3.wagons
 train3.remove_wagon
+train4.remove_wagon
 puts train3.wagons
 
 train3.add_route(route1)
-puts train3.route.get_stations
+puts train3.route.list_stations
 puts train3.current_station.station_name
-train3.current_station.get_trains
+train3.current_station.trains
 train3.to_next_station
 puts train3.current_station.station_name
-train3.current_station.get_trains
+train3.current_station.trains
 train3.to_next_station
 puts train3.current_station.station_name
-train3.current_station.get_trains
+train3.current_station.trains
 train3.to_previous_station
 puts train3.current_station.station_name
-train3.current_station.get_trains
-train3.get_current_station
+train3.current_station.trains
+
+train3.increase_speed(50)
+puts train3.speed
+train3.reduse_speed(20)
+puts train3.speed
+train3.reduse_speed(50)
+puts train3.speed
